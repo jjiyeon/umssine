b6a4f8dbfa521dc87b94fc63e2ea12bce9bcc38d
Journey <ejy1105@gmail.com>
2022-12-31 17:35:34 +0900

    style: 헤더 스타일 추가

--- components/MyInfo/index.tsx
+++ components/MyInfo/index.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components'
 
 const MyInfo = () => {
   return (
-    <div>
+    <StyledMyInfoContainer>
       <Image width={150} height={150} src="/dal2.png" alt="main image" />
-      <p>엄씨네 친구들</p>
-    </div>
+      <StyledMainInfo>엄씨네 친구들</StyledMainInfo>
+    </StyledMyInfoContainer>
   )
 }
 

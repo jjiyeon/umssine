3802075ed0ec3f0333ef2feec0dc56b0ae87da33
Journey <ejy1105@gmail.com>
2023-01-07 22:27:06 +0900

    chore: 폰트 리소스 설정 및 head 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
-import type { AppProps } from "next/app";
-import GlobalStyle from "../styles/GlobalStyle";
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyle from '../styles/GlobalStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Umssine</title>
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,19 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap"
-          />
-          <title>Umssine</title>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument

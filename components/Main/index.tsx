a9b6629984eb31e6f228a678afbd77a1f3417462
Journey <ejy1105@gmail.com>
2023-01-07 22:27:53 +0900

    Feat: next image 컴포넌트 추가

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 
-const Images = [
+const MockImages = [
   { id: 1, url: '/mock/1.JPG' },
   { id: 2, url: '/mock/2.JPG' },
   { id: 3, url: '/mock/3.JPG' },
@@ -13,9 +13,19 @@ const Main = () => {
       <StyledMainWrapper>
         <StyledImageSlider>
           <StyledImageList>
-            {Images.map((item, index) => (
-              <StyledImageItem>
-                <Image src={item.url} alt="my photo" fill={true} />
+            {MockImages.map((item, _) => (
+              <StyledImageItem key={item.id}>
+                <Image
+                  src={item.url}
+                  width={300}
+                  height={300}
+                  alt="my photo"
+                  style={{
+                    maxWidth: '100%',
+                    objectFit: 'cover',
+                    objectPosition: 'center',
+                  }}
+                />
               </StyledImageItem>
             ))}
           </StyledImageList>
@@ -32,11 +42,14 @@ const StyledMainWrapper = styled.div``
 const StyledImageSlider = styled.div``
 const StyledImageList = styled.ul`
   display: flex;
-  overflow: hidden;
+  flex-wrap: wrap;
   width: 300px;
-  height: 600px;
+  height: 300px;
+  overflow: hidden;
 `
 const StyledImageItem = styled.li`
-  object-fit: contain;
+  position: relative;
+  width: 100%;
+  height: 100%;
 `
 const StyledText = styled.p``
